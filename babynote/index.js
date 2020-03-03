c4f73f6d21bcbf99bd9d3810bc5a0a26705a0df9
Hyunse <48168860+rain2353@users.noreply.github.com>
2020-03-03 17:24:30 +0900

    babynote_server

--- babynote/index.js
+++ babynote/index.js
@@ -9,8 +9,8 @@ var newDate = require('date-utils');
 //Connect to MySQL
 var con = mysql.createConnection({
     host: 'localhost',
-    user: '@@@@@@@@',
-    password: '@@@@@@@@',
+    user: '@@@@@@@@@',
+    password: '@@@@@@@@@',
     database: 'babynote'
 });
 
